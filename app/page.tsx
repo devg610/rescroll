ef6f8aadf8a5361d4861dc8c7e2d6d8ae58327fc
devg610 <devlingrelle@gmail.com>
2026-05-27 23:20:41 -0400

    split logo into icon and wordmark

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       className="flex flex-1 flex-col items-center justify-center px-6"
       style={{ backgroundColor: "var(--background)", color: "var(--foreground)" }}
     >
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-2">
         {/* Icon - static */}
         <Image
           src="/logo-black.svg"
